57a62f3af3005d89e0f07a1b972bc6e38652f16a
glozanoa <glozanoa@uni.com>
2022-05-09 16:27:17 -0500

    adding nasty code

--- injection.py
+++ injection.py
@@ -1,9 +1,19 @@
 #!/usr/bin/env python3
 
-# Testing if guardrailds catch this posible command injection
+# Testing if guardrailds catch these vulnerabilities
 
 import os
+import 
 
 
 def shell(cmd):
+    # possible command injection
     os.system(cmd)
+
+def getSomething(query):
+    # possible SQLI and data exposure
+    conn = psycopg2.connect(host='localhost', database='secretdb', user='root', password='$UperP4$vv0rd')
+    cur = conn.cursor()
+    cur.execute(query)
+
+    # SOMEHTING MORE
